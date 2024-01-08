3ea82bd5d97cbadf755f1e0625db5db05fad6e1b
trilvid <trilztech34@gmail.com>
2024-01-08 10:52:33 -0800

    updated route

--- src/components/invesments/Investments.jsx
+++ src/components/invesments/Investments.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 import { useNavigate,Link } from 'react-router-dom'
 import Userdashboardheader from '../userdashboardheader/Userdashboardheader'
+import Loader from '../Loader'
 const Investments = ({route}) => {
 
   
@@ -33,19 +34,7 @@ const Investments = ({route}) => {
     <Userdashboardheader route={route}/>
     {
         loader && 
-          <div className="wifi-loader-container">
-            <div class="loader">
-              <span class="l">L</span>
-              <span class="o">o</span>
-              <span class="a">a</span>
-              <span class="d">d</span>
-              <span class="i">i</span>
-              <span class="n">n</span>
-              <span class="g">g</span>
-              <span class="d1">.</span>
-              <span class="d2">.</span>
-            </div>
-        </div>
+          <Loader />
       }
       {userData && userData.invest.length !== 0 ? 
       <div className="page-swiper-wrapper">
